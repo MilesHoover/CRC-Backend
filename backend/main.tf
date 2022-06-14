4553e15e3a1b537d4eb4b076e20562e3fdb525a1
Miles Hoover <mhoover708@gmail.com>
2022-06-13 20:57:55 -0400

    update resource names & add Cloudfront Distribution

--- backend/main.tf
+++ backend/main.tf
@@ -14,13 +14,13 @@ provider "aws" {
 }
 
 # # Creates S3 bucket
-resource "aws_s3_bucket" "s3-bucket" {
+resource "aws_s3_bucket" "s3_bucket" {
   bucket = "mileshoover.com"
 }
 
 # Configures S3 options
-resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.s3-bucket.bucket
+resource "aws_s3_bucket_website_configuration" "s3_config" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
 
   index_document {
     suffix = "index.html"
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_website_configuration" "example" {
 
 # Sets ACL policy
 resource "aws_s3_bucket_acl" "s3-bucket-acl" {
-  bucket = aws_s3_bucket.s3-bucket.id
+  bucket = aws_s3_bucket.s3_bucket.id
   acl    = "public-read"
 }
 
@@ -52,8 +52,8 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
         "s3:GetObject"
       ],
       "Resource": [
-        "${aws_s3_bucket.s3-bucket.arn}/*",
-        "${aws_s3_bucket.s3-bucket.arn}"
+        "${aws_s3_bucket.s3_bucket.arn}/*",
+        "${aws_s3_bucket.s3_bucket.arn}"
       ]
     }
   ]
@@ -64,13 +64,55 @@ POLICY
 # Adds website objects to S3
 # Feature is not currently working
 
-/*resource "aws_s3_object" "objects" {
+/*resource "aws_s3_object" "s3_objects" {
   for_each = fileset("website/", "**")
   bucket = aws_s3_bucket.s3-bucket.id
   key = each.value
   source = "website/${each.value}"
   etag = filemd5("website/${each.value}")
 }*/
+
+# # Cloudfront Distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
 
 
 
