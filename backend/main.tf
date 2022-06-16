fb887b360163d8220285a8bf694e0eaba83b7610
Miles Hoover <mhoover708@gmail.com>
2022-06-16 17:23:19 -0400

    Completed API

--- backend/main.tf
+++ backend/main.tf
@@ -232,3 +232,54 @@ resource "aws_api_gateway_integration" "api_integration" {
   uri              = aws_lambda_function.lambda.invoke_arn
   content_handling = "CONVERT_TO_TEXT"
 }
+
+# API method response
+resource "aws_api_gateway_method_response" "api_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.counter_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = aws_api_gateway_method.api_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+# API integration response
+resource "aws_api_gateway_integration_response" "api_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.counter_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = aws_api_gateway_method.api_method.http_method
+  status_code = aws_api_gateway_method_response.api_method_response.status_code
+  response_parameters {
+    "method.response.header.Access-Control-Allow-Methods" = "'GET, POST'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+# API deployment
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.counter_api.id
+  triggers {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.api_resource.id,
+      aws_api_gateway_method.api_method.id,
+      aws_api_gateway_integration.api_integration.id,
+    ]))
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# API stage
+resource "aws_api_gateway_stage" "api_stage" {
+  rest_api_id   = aws_api_gateway_rest_api.counter_api.id
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  stage_name    = "counterapi"
+}
