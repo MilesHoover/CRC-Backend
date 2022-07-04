49d0e3b350a1bd8b40dda5fa0a53e7fdb99d93f2
Miles Hoover <mhoover708@gmail.com>
2022-07-04 12:16:19 -0400

    backend complete

--- cypress/e2e/api_counter_test.cy.js
+++ cypress/e2e/api_counter_test.cy.js
@@ -1,6 +1,6 @@
 describe('counter_api_test', () => {
   it('GET', () => {
-      cy.request('GET', 'https://ak2nkurklj.execute-api.us-east-1.amazonaws.com/prod/count').then((response) => {
+      cy.request('GET', 'https://l4rbd59ypk.execute-api.us-east-1.amazonaws.com/prod/count').then((response) => {
           expect(response).to.have.property('status', 200)
           expect(response.body).to.not.be.null
           expect(response.body).to.be.a('number')
